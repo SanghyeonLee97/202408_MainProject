94bafea17808dfec9ccfd458742cd05b133e6bd7
SanghyeonLee97 <ledlee.97@gmail.com>
2024-09-04 13:04:33 +0900

    성별, 연령대별 뷰 생성 코드 추가

--- DATABASE/view 생성파일.sql
+++ DATABASE/view 생성파일.sql
@@ -55,7 +55,72 @@ GROUP BY
 ORDER BY
     like_count DESC;
 
+-- 모든 성별에 대한 좋아요를 합쳐서 반환하는 뷰
+CREATE VIEW cafe_likes_bygender AS
+SELECT
+    C.CAFE_ID,
+    C.CAFE_NAME,
+    C.IMAGE_URL,
+    M.GENDER,
+    COUNT(MA.GOOD) AS like_count,
+    MA.MOD_DATE AS mod_date_genderlikes
+FROM
+    member_act MA
+JOIN
+    member M ON MA.MEMBER_ID = M.MEMBER_ID
+JOIN
+    cafe C ON MA.CAFE_ID = C.CAFE_ID
+WHERE
+    MA.GOOD = 'Y'
+GROUP BY
+    C.CAFE_ID, C.CAFE_NAME, C.IMAGE_URL, M.GENDER
+ORDER BY
+    like_count DESC;
 
+-- 연령대 별 좋아요를 합쳐서 반환하는 뷰
+CREATE VIEW cafe_likes_byage AS
+SELECT
+    C.CAFE_ID,
+    C.CAFE_NAME,
+    C.IMAGE_URL,
+    CASE 
+        -- YY가 00 이상인 경우는 2000년대, 그 외에는 1900년대
+        WHEN FLOOR((YEAR(CURDATE()) - (CASE 
+            WHEN LEFT(M.BIRTHDATE, 2) <= '23' THEN CONCAT('20', LEFT(M.BIRTHDATE, 2))
+            ELSE CONCAT('19', LEFT(M.BIRTHDATE, 2))
+        END)) / 10) * 10 = 10 THEN '10대'
+        WHEN FLOOR((YEAR(CURDATE()) - (CASE 
+            WHEN LEFT(M.BIRTHDATE, 2) <= '23' THEN CONCAT('20', LEFT(M.BIRTHDATE, 2))
+            ELSE CONCAT('19', LEFT(M.BIRTHDATE, 2))
+        END)) / 10) * 10 = 20 THEN '20대'
+        WHEN FLOOR((YEAR(CURDATE()) - (CASE 
+            WHEN LEFT(M.BIRTHDATE, 2) <= '23' THEN CONCAT('20', LEFT(M.BIRTHDATE, 2))
+            ELSE CONCAT('19', LEFT(M.BIRTHDATE, 2))
+        END)) / 10) * 10 = 30 THEN '30대'
+        WHEN FLOOR((YEAR(CURDATE()) - (CASE 
+            WHEN LEFT(M.BIRTHDATE, 2) <= '23' THEN CONCAT('20', LEFT(M.BIRTHDATE, 2))
+            ELSE CONCAT('19', LEFT(M.BIRTHDATE, 2))
+        END)) / 10) * 10 = 40 THEN '40대'
+        WHEN FLOOR((YEAR(CURDATE()) - (CASE 
+            WHEN LEFT(M.BIRTHDATE, 2) <= '23' THEN CONCAT('20', LEFT(M.BIRTHDATE, 2))
+            ELSE CONCAT('19', LEFT(M.BIRTHDATE, 2))
+        END)) / 10) * 10 = 50 THEN '50대'
+        ELSE '60대 이상'
+    END AS AGE_GROUP,
+    COUNT(MA.GOOD) AS like_count,
+    MA.MOD_DATE AS mod_date_agelikes
+FROM
+    member_act MA
+JOIN
+    member M ON MA.MEMBER_ID = M.MEMBER_ID
+JOIN
+    cafe C ON MA.CAFE_ID = C.CAFE_ID
+WHERE
+    MA.GOOD = 'Y'
+GROUP BY
+    C.CAFE_ID, C.CAFE_NAME, C.IMAGE_URL, AGE_GROUP
+ORDER BY
+    like_count DESC;
 
 ------------------------------------------------------
 -- 사용 예시 코드

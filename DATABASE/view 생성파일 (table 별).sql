f1e06fadb55a12ee87601442d50630e81995c959
SanghyeonLee97 <ledlee.97@gmail.com>
2024-09-05 16:28:33 +0900

    신규 코드 추가

---
+++ DATABASE/view 생성파일 (table 별).sql
@@ -0,0 +1,27 @@
+CREATE VIEW vw_member_like AS
+SELECT 
+    member_id, 
+    cafe_type, 
+    mood, 
+    mod_date AS mod_date_ml
+FROM member_like;
+
+CREATE VIEW vw_cafe AS
+SELECT 
+    cafe_id, 
+    cafe_name, 
+    cafe_type, 
+    mood, 
+    image_url, 
+    mod_date AS mod_date_c
+FROM cafe;
+
+CREATE VIEW vw_member_act AS
+SELECT 
+	member_id,
+    cafe_id, 
+    good,
+    review,
+    mood,
+    mod_date AS mod_date_ma
+FROM member_act;

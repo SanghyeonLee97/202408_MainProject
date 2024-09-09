85c26292b3bdd1f18ddc867bcec1eae600e28d9a
SanghyeonLee97 <ledlee.97@gmail.com>
2024-09-09 17:51:35 +0900

    Update [더미데이터 생성함수] MEMBER 테이블.sql

--- DATABASE/더미데이터 생성함수 모음/[더미데이터 생성함수] MEMBER 테이블.sql
+++ DATABASE/더미데이터 생성함수 모음/[더미데이터 생성함수] MEMBER 테이블.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE generate_dummy_members()
 BEGIN
     DECLARE i INT DEFAULT 1;
     DECLARE korean_last_names VARCHAR(255) DEFAULT '김,이,박,최,정,강,조,윤,장,임,오,한,신,서,권,황,안,송,류,홍';
-    DECLARE korean_first_names VARCHAR(255) DEFAULT '민수,지우,서현,지민,하준,서준,예은,지호,수현,유진,하윤,준서,도윤,예린,윤서';
+    DECLARE korean_first_names VARCHAR(255) DEFAULT '민수,지우,서현,지민,하준,서준,예은,지호,수현,유진,하윤,준서,도윤,예린,윤서,현우,지안,민재,지후,은서,재윤,수빈,지유,하늘,태현,서영,준호,소윤,시우,주원,아윤,서영,예진,진우,서진,민채,유림,정우,지연,연우,우진,채원,지유,서율,연서,지후';
     
     -- 성과 이름을 저장할 테이블 생성
     CREATE TEMPORARY TABLE IF NOT EXISTS temp_names (
@@ -34,7 +34,7 @@ BEGIN
     ) n
     WHERE n.n <= 1 + (LENGTH(korean_first_names) - LENGTH(REPLACE(korean_first_names, ',', '')));
 
-    WHILE i <= 100 DO
+    WHILE i <= 500 DO
         -- 랜덤 한국 성과 이름 선택
         SET @last_name = (SELECT last_name FROM temp_names WHERE last_name IS NOT NULL ORDER BY RAND() LIMIT 1);
         SET @first_name = (SELECT first_name FROM temp_names WHERE first_name IS NOT NULL ORDER BY RAND() LIMIT 1);
@@ -51,15 +51,15 @@ BEGIN
         SET @dummy_email = CONCAT('user', i, '@example.com');
         SET @dummy_pw = '1234';  -- 비밀번호를 1234로 고정
         
-        -- 전화번호를 000-0000-0000 형식으로 생성
+        -- 전화번호를 010-0000-0000 형식으로 생성
         SET @dummy_tel = CONCAT(
             '010', '-',
             LPAD(FLOOR(RAND() * 10000), 4, '0'), '-',
             LPAD(FLOOR(RAND() * 10000), 4, '0')
         );
         
-        -- 생년월일을 YYMMDD 형식으로 생성 (1970년부터 2010년 사이)
-        SET @year = LPAD(FLOOR(70 + (RAND() * 41)), 2, '0');  -- 1970~2010년의 두 자리 연도
+        -- 생년월일을 YYMMDD 형식으로 생성 (1960년부터 2010년 사이)
+        SET @year = LPAD(FLOOR(60 + (RAND() * 51)), 2, '0');  -- 1960~2010년의 두 자리 연도
         SET @month = LPAD(FLOOR(1 + RAND() * 12), 2, '0');    -- 1~12월
         SET @day = LPAD(FLOOR(1 + RAND() * 28), 2, '0');      -- 1~28일 (단순화)
         SET @dummy_birthdate = CONCAT(@year, @month, @day);   -- YYMMDD 형식의 생일
